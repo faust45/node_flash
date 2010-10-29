23cb1df704a328552720def4d8778f154fad8ff2
faust45 <faust451@gmail.com>
2010-10-29 18:21:30 +0600

    Refactoring

--- imgs.js
+++ imgs.js
@@ -1,18 +1,46 @@
-var temp = require('temp');
-var exec = require('child_process').exec;
+var temp = require('temp'),
+    gm = require('gm'),
+    EventEmitter = require('events').EventEmitter,
+    exec = require('child_process').exec;
 
-exports.roundedCorners = function(fileName, fun) {
+
+exports.process = function(filePath, options) {
+  return new ImgProcessor(filePath, options);
+};
+
+
+//Private------------------------------------------------------------
+function ImgProcessor(filePath, options) {
+  options = options || {};
+  var self = this;
+  var size = options.size || {width: 100, height: 100};
+  var newFile = temp.path({suffix: '.png'});
+
+  gm(filePath)
+   .resize(size.width, size.height)
+   .write(newFile, function(err) {
+       if(!err) {
+         roundedCorners(newFile, function(finishFile) {
+           self.emit('finish', finishFile, newFile);
+         }); 
+       } else {
+         console.log(err);
+       }
+    });
+
+  return this;
+}
+
+ImgProcessor.prototype.__proto__ = EventEmitter.prototype;
+
+roundedCorners = function(filePath, cb) {
   var newFile = temp.path();
-  var cmd = './rounded_corners.sh ' + fileName + ' ' + newFile;
+  var cmd = './rounded_corners.sh ' + filePath + ' ' + newFile;
   console.log(cmd);
 
   exec(cmd, function(error, stdout, stderr) {
     console.log(error);
     console.log(stderr);
-    fun(newFile);
+    cb(newFile);
   });
-}
-
-exports.toPng = function(fileName) {
-  return fileName.replace(/\.\w*$/, '.png');
 }
--- serv.js
+++ serv.js
@@ -2,7 +2,6 @@ var http = require('http'),
     httpProxy = require('http-proxy'),
     url = require('url'),
     fs = require('fs'),
-    gm = require('gm'),
     sys = require('util'),
     utils = require('connect/utils'),
     imgs = require('./imgs'),
@@ -13,37 +12,31 @@ httpProxy.createServer(function (req, res, proxy) {
   var path = url.parse(req.url),
       id = path.pathname.replace('/', '');
       params = qs.parse(path.query),
-      defaultSize = {'width': 200, 'height': 200};
       size = parseSize(params.size) || defaultSize;
 
   if (id == 'favicon.ico') {
-     return;
+    return;
   } 
 
   myCouch.downloadAttachment(id, {}, function(er, filePath) {
     p('download complete');
     p(filePath);
-    var pngFile = imgs.toPng(filePath);
-    gm(filePath)
-      .resize(size.width, size.height)
-      .write(pngFile, function(err) {
-          if(!err) {
-            //imgs.roundedCorners(pngFile, function(newFile) {
-              res.writeHead(200, {
-                'Content-Type': utils.mime.type(pngFile)
-              });
-              var readStream = fs.createReadStream(pngFile, {'encoding': 'binary'});
 
-              readStream.on('data', function (chunk) {
-                res.write(chunk, 'binary');
-              });
-              readStream.on('end', function () {
-                res.end();
-              });
-
-            //}); 
-          }
+    var processor = imgs.process(filePath, {size: size});
+    processor.on('finish', function(filePath, fileName) {
+      p('finish');
+      res.writeHead(200, {
+        'Content-Type': utils.mime.type(fileName)
       });
+      var readStream = fs.createReadStream(filePath, {'encoding': 'binary'});
+
+      readStream.on('data', function (chunk) {
+        res.write(chunk, 'binary');
+      });
+      readStream.on('end', function () {
+        res.end();
+      });
+    });
   });
 
   //req.url = '/' + 'cache' + '/' + id + '/' + fileName;
